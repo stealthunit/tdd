759d4dfb78bbc5cfbb9af82d5dae50560116f660
stealthunit <nick.stels.unit@gmail.com>
2017-09-11 16:48:42 +0500

    начата разработка для точек больше, чем одной

--- TagsCloudVisualization/CircularCloudLayouter.cs
+++ TagsCloudVisualization/CircularCloudLayouter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,7 @@ namespace TagsCloudVisualization
     class CircularCloudLayouter
     {
         private Point _center = new Point();
-        List<Rectangle> allFigures = new List<Rectangle>();
+        public List<Rectangle> allFigures = new List<Rectangle>();
         public CircularCloudLayouter(Point center)
         {
             _center = center;
@@ -19,9 +20,59 @@ namespace TagsCloudVisualization
 
         public Rectangle PutNextRectangle(Size rectangleSize)
         {
-            Rectangle newRectangle = new Rectangle(_center, rectangleSize);
+            Point nextRectangleCenter = GetNewRectangleCoords(rectangleSize);
+            Rectangle newRectangle = new Rectangle(nextRectangleCenter, rectangleSize);
             allFigures.Add(newRectangle);
             return newRectangle;
+        }
+
+        private Point GetNewRectangleCoords(Size rectangleSize)
+        {
+            if (allFigures.Count == 0)
+                return new Point(_center.X - rectangleSize.Width / 2, _center.Y - rectangleSize.Height / 2);
+            int radius = allFigures.Max(i => i.Height) + rectangleSize.Height;
+            // циклом идти по окружности и искать точку
+            for (int step = -radius; step <= radius; step++)
+            {
+                int x1 = radius * (int)Math.Cos(radius) + _center.X;
+                int y1 = radius * (int)Math.Sin(radius) + _center.Y;
+                if (!CheckAreaIsFilled(new Point(x1, y1), rectangleSize))
+                    return new Point(x1 - rectangleSize.Width/2,y1 - rectangleSize.Height / 2);
+            }
+            return new Point();
+        }
+
+        /// <summary>
+        /// Проверяет, свободно ли пространство под ключевыми точками, куда хотим поставить фигуру
+        /// </summary>
+        /// <param name="supposedCenter"></param>
+        /// <param name="rectangleSize"></param>
+        /// <returns></returns>
+        private bool CheckAreaIsFilled(Point supposedCenter, Size rectangleSize)
+        {
+            if (!CheckPointIsFilled(supposedCenter) &&
+                !CheckPointIsFilled(new Point(supposedCenter.X + rectangleSize.Width / 2)) &&
+                !CheckPointIsFilled(new Point(supposedCenter.X - rectangleSize.Width / 2)) &&
+            !CheckPointIsFilled(new Point(supposedCenter.Y + rectangleSize.Height / 2)) &&
+            !CheckPointIsFilled(new Point(supposedCenter.Y - rectangleSize.Height / 2)))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Проверяет, занято ли другой фигурой место, куда хотим поставить точку
+        /// </summary>
+        /// <param name="exPoint"></param>
+        /// <returns></returns>
+        private bool CheckPointIsFilled(Point exPoint)
+        {
+            return allFigures.Any(i =>
+                i.X <= exPoint.X &&
+                i.X + i.Width >= exPoint.X &&
+                i.Y <= exPoint.Y &&
+                i.Y + i.Height >= exPoint.Y);
         }
         //private double DistancePointFromCenter(int x, int y)
         //{
@@ -39,10 +90,23 @@ namespace TagsCloudVisualization
         {
             var cloud = new CircularCloudLayouter(new Point(100, 100));
             var figure = cloud.PutNextRectangle(new Size(50,20));
-            Assert.AreEqual(figure.X, 100);
-            Assert.AreEqual(figure.Y, 100);
+            Assert.AreEqual(75, figure.X);
+            Assert.AreEqual(90, figure.Y);
             Assert.AreEqual(figure.Width, 50);
             Assert.AreEqual(figure.Height, 20);
+        }
+
+        [TestMethod]
+        public void PutFirstLevelNeigbors()
+        {
+            var cloud = new CircularCloudLayouter(new Point(100, 100));
+            cloud.PutNextRectangle(new Size(50, 20));
+            var figure2 = cloud.PutNextRectangle(new Size(40, 16));
+            Assert.AreEqual(2, cloud.allFigures.Count);
+            Assert.AreEqual(100, figure2.X);
+            Assert.AreEqual(72, figure2.Y);
+            Assert.AreEqual(40, figure2.Width);
+            Assert.AreEqual(16, figure2.Height);
         }
     }
 }
